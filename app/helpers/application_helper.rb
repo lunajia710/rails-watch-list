4f5c3d9eb48f2ab81f74129300a78cf6df1e8d1c
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-11 18:42:33 +1000

    basic styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def star_rating(rating)
+    return "" if rating.blank?
+
+    full_stars = (rating.to_f / 2).round.clamp(0, 5)
+    safe_join(
+      Array.new(5) { |i| content_tag(:i, "", class: i < full_stars ? "fa-solid fa-star" : "fa-regular fa-star") }
+    )
+  end
 end
--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,12 @@
 module ListsHelper
+  COVER_IMAGES = %w[
+    covers/cover1.avif
+    covers/cover2.avif
+    covers/cover3.avif
+    covers/cover4.avif
+  ].freeze
+
+  def list_cover_image(list)
+    COVER_IMAGES[list.id % COVER_IMAGES.size]
+  end
 end
