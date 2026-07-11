1e12dc104d42fa18a148d44a54d4be9ed3cf97b6
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-11 13:37:31 +1000

    update list/movie pair validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id, message: "has already added to this list" }
+  validates :list_id, uniqueness: { scope: :movie_id, message: "has already added to this list" }
   belongs_to :list
   belongs_to :movie
 end
